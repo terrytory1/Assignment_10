15e4bfd2404a200bfa08e3c1831cacb786f1a7fe
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:38:41 +0900

    train_file_list_to_json implemented

--- main.py
+++ main.py
@@ -12,27 +12,23 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     """Converts two lists of file paths into a list of json strings"""
     # Preprocess unwanted characters
     def process_file(file):
-        if '\\' in file:
-            file = file.replace('\\', '\\\\')
-        if '/' or '"' in file:
-            file = file.replace('/', '\\/')
-            file = file.replace('"', '\\"')
+        file = file.replace('\\', '\\\\')
+        file = file.replace('/', '\\/')
+        file = file.replace('"', '\\"')
         return file
 
     # Template for json file
-    template_start = '{\"English\":\"'
-    template_mid = '\",\"German\":\"'
-    template_end = '\"}'
+    template_start = '{"English":"'
+    template_mid = '","German":"'
+    template_end = '"}'
 
-    # Can this be working?
     processed_file_list = []
     for english_file, german_file in zip(english_file_list, german_file_list):
         english_file = process_file(english_file)
-        english_file = process_file(german_file)
+        german_file = process_file(german_file)
 
-        processed_file_list.append(template_end + english_file + template_mid + german_file + template_start)
+        processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
     return processed_file_list
-
 
 def write_file_list(file_list: List[str], path: str) -> None:
     """Writes a list of strings to a file, each string on a new line"""
